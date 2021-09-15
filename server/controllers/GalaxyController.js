46fb4d610fd97e75befcddb7cdf7cfb8c2a1753d
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-15 15:39:27 -0600

    galaxy service and controller done

--- server/controllers/GalaxyController.js
+++ server/controllers/GalaxyController.js
@@ -1,9 +1,60 @@
 import BaseController from '../utils/BaseController'
-import { galaxyService } from '../services/GalaxyService.js'
+import { galaxysService } from '../services/GalaxysService.js'
+import { logger } from '../utils/Logger.js'
 
 export class GalaxyController extends BaseController {
   constructor() {
     super('api/Galaxy')
     this.router
+      .get('', this.getGalaxys)
+      .get('', this.getGalaxy)
+      .post('', this.createGalaxy)
+      .put('', this.editGalaxy)
+      .delete('/:galaxyId', this.removeGalaxy)
+  }
+
+  async getGalaxys(req, res, next) {
+    try {
+      const galaxys = await galaxysService.getGalaxy(req.query)
+      res.send(galaxys)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getGalaxy(req, res, next) {
+    try {
+      const galaxy = await galaxysService.getGalaxyById(req.params.galaxyId)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async createGalaxy(req, res, next) {
+    try {
+      const galaxy = await galaxysService.createGalaxy(req.body)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async removeGalaxy(req, res, next) {
+    try {
+      const galaxy = await galaxysService.removeGalaxy(req.params.galaxyId)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editGalaxy(req, res, next) {
+    try {
+      const galaxy = await galaxysService.editGalaxy(req.params.galaxyId, req.body)
+      res.send(galaxy)
+    } catch (error) {
+      next(error)
+    }
   }
 }
--- server/services/GalaxysService.js
+++ server/services/GalaxysService.js
@@ -1,10 +1,38 @@
 import { dbContext } from '../db/DbContext.js'
-import { BadRequest, Forbidden } from '../utils/Errors.js'
+import { BadRequest } from '../utils/Errors.js'
 
-Class GalaxyService {
+class GalaxysService {
+  async getGalaxyById(galaxyId) {
+    const galaxy = await dbContext.Galaxys.findById(galaxyId).populate('creator', 'name picture')
+    if (!galaxy) {
+      throw new BadRequest('Invalid galaxy Id')
+    }
+    return galaxy
+  }
 
+  async editGalaxy(galaxyId, galaxyData) {
+    const galaxy = await this.getGalaxyById(galaxyId)
+    galaxy.description = galaxyData.description || galaxy.description
+    galaxy.name = galaxyData.name || galaxy.name
+    await galaxy.save()
+    return galaxy
+  }
+
+  async createGalaxy(galaxyData) {
+    const galaxy = await dbContext.Galaxys.create(galaxyData)
+    return galaxy
+  }
+
+  async removeGalaxy(galaxyId) {
+    const galaxy = await this.getGalaxyById(galaxyId)
+    await galaxy.remove()
+    return galaxy
+  }
+
+  async getGalaxys(query) {
+    const galaxys = await dbContext.Galaxys.find(query).populate('creator', 'name picture')
+    return galaxys
+  }
 }
 
-
-
-export const galaxyService = new GalaxyService()
+export const galaxysService = new GalaxysService()
