73bfb4e991ab6f070aa6d2f206304fdc013fcf0c
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-15 14:40:05 -0600

    Dbcontext filled out

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,8 +1,16 @@
 import mongoose from 'mongoose'
 import { ValueSchema } from '../models/Value'
+import { StarSchema } from '../models/Star'
+import { GalaxySchema } from '../models/Galaxy'
+import { MoonSchema } from '../models/Moon'
+import { PlanetSchema } from '../models/Planet'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
+  Stars = mongoose.model('Star', StarSchema);
+  Galaxy = mongoose.model('Galaxy', GalaxySchema);
+  Planets = mongoose.model('Planet', PlanetSchema);
+  Moons = mongoose.model('Moon', MoonSchema);
 }
 
 export const dbContext = new DbContext()
